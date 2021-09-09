1ee5d1b226384300143e83dbda76e1b41fe6339f
Hongkai <hongkailo2000@gmail.com>
2021-09-10 00:28:08 +0800

    Added check internet connection feature

--- combine.py
+++ combine.py
@@ -54,10 +54,7 @@ class Login(QDialog):
     def __init__(self):
         super(Login, self).__init__()
         loadUi("ui/login.ui", self)
-        if is_internet():
-            print("Yes")
-        # self.email.setPlaceholderText("Email...")
-        # self.password.setPlaceholderText("Password...")
+
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
 
         self.loginButton.clicked.connect(self.loginfunction)
@@ -66,28 +63,33 @@ class Login(QDialog):
         self.errorMessage.setVisible(False)
 
     def loginfunction(self):
-        email = self.email.text()
-        password = self.password.text()
-        # Check is empty
-        if not email or not password:
-            self.errorMessage.setText("All fields cannot be empty.")
+        # Check internet connection
+        if not is_internet():
+            self.errorMessage.setText("Please check you internet connection or try again later.")
             self.errorMessage.setVisible(True)
         else:
-            # Validate the email
-            regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-            if not re.fullmatch(regex, email):
-                self.errorMessage.setText("Invalid email format.")
+            email = self.email.text()
+            password = self.password.text()
+            # Check is empty
+            if not email or not password:
+                self.errorMessage.setText("All fields cannot be empty.")
                 self.errorMessage.setVisible(True)
             else:
-                try:
-                    # login here
-                    auth.sign_in_with_email_and_password(email, password)
-                    # After login
-                    print("Successfully logged in with email: ", email, "and Password: ", password)
-                    self.gotoDetectorScreen(email)
-                except:
-                    self.errorMessage.setText("Incorrect email or password.")
+                # Validate the email
+                regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+                if not re.fullmatch(regex, email):
+                    self.errorMessage.setText("Invalid email format.")
                     self.errorMessage.setVisible(True)
+                else:
+                    try:
+                        # login here
+                        auth.sign_in_with_email_and_password(email, password)
+                        # After login
+                        print("Successfully logged in with email: ", email, "and Password: ", password)
+                        self.gotoDetectorScreen(email)
+                    except:
+                        self.errorMessage.setText("Incorrect email or password.")
+                        self.errorMessage.setVisible(True)
 
     def gotocreate(self):
         createacc = CreateAcc()
@@ -114,57 +116,62 @@ class CreateAcc(QDialog):
         self.errorMessage.setVisible(False)
 
     def createaccfunction(self):
-        email = self.email.text()
-        input_password = self.password.text()
-        confirmPassword = self.confirmPassword.text()
-        # Check is empty
-        if not email or not input_password or not confirmPassword:
-            self.errorMessage.setText("All fields cannot be empty.")
+        # Check internet connection
+        if not is_internet():
+            self.errorMessage.setText("Please check you internet connection or try again later.")
             self.errorMessage.setVisible(True)
         else:
-            # Validate the email
-            regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-            if not re.fullmatch(regex, email):
-                self.errorMessage.setText("Invalid email format.")
+            email = self.email.text()
+            input_password = self.password.text()
+            confirmPassword = self.confirmPassword.text()
+            # Check is empty
+            if not email or not input_password or not confirmPassword:
+                self.errorMessage.setText("All fields cannot be empty.")
                 self.errorMessage.setVisible(True)
             else:
-                # Compare password
-                if input_password == confirmPassword:
-                    password = confirmPassword
-                    # validate password
-                    if len(password) < 8:
-                        self.errorMessage.setText("Password min length is 8.")
-                        self.errorMessage.setVisible(True)
-                    elif re.search('[0-9]', password) is None:
-                        self.errorMessage.setText("Password must has at least a digit.")
-                        self.errorMessage.setVisible(True)
-                    elif re.search('[A-Z]', password) is None:
-                        self.errorMessage.setText("Password must has at least 1 uppercase letter.")
-                        self.errorMessage.setVisible(True)
-                    elif re.search('[a-z]', password) is None:
-                        self.errorMessage.setText("Password must has at least 1 lowercase letter.")
-                        self.errorMessage.setVisible(True)
-                    elif re.search('[@#$%^&*?]', password) is None:
-                        self.errorMessage.setText("Password must has at least 1 special symbols.")
-                        self.errorMessage.setVisible(True)
-                    else:
-                        try:
-                            # Register here
-                            auth.create_user_with_email_and_password(email, password)
-                            print("Successfully registered with email:", email, "and Password:", password)
-                            msg = QtWidgets.QMessageBox()
-                            msg.setIcon(QtWidgets.QMessageBox.Information)
-                            msg.setText("Account has been successfully created.")
-                            msg.setWindowTitle("Register")
-                            msg.exec_()
-
-                            self.gotologin()
-                        except:
-                            self.errorMessage.setText("Email has already been taken.")
-                            self.errorMessage.setVisible(True)
-                else:
-                    self.errorMessage.setText("Passwords doesn't match!")
+                # Validate the email
+                regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+                if not re.fullmatch(regex, email):
+                    self.errorMessage.setText("Invalid email format.")
                     self.errorMessage.setVisible(True)
+                else:
+                    # Compare password
+                    if input_password == confirmPassword:
+                        password = confirmPassword
+                        # validate password
+                        if len(password) < 8:
+                            self.errorMessage.setText("Password min length is 8.")
+                            self.errorMessage.setVisible(True)
+                        elif re.search('[0-9]', password) is None:
+                            self.errorMessage.setText("Password must has at least a digit.")
+                            self.errorMessage.setVisible(True)
+                        elif re.search('[A-Z]', password) is None:
+                            self.errorMessage.setText("Password must has at least 1 uppercase letter.")
+                            self.errorMessage.setVisible(True)
+                        elif re.search('[a-z]', password) is None:
+                            self.errorMessage.setText("Password must has at least 1 lowercase letter.")
+                            self.errorMessage.setVisible(True)
+                        elif re.search('[@#$%^&*?]', password) is None:
+                            self.errorMessage.setText("Password must has at least 1 special symbols.")
+                            self.errorMessage.setVisible(True)
+                        else:
+                            try:
+                                # Register here
+                                auth.create_user_with_email_and_password(email, password)
+                                print("Successfully registered with email:", email, "and Password:", password)
+                                msg = QtWidgets.QMessageBox()
+                                msg.setIcon(QtWidgets.QMessageBox.Information)
+                                msg.setText("Account has been successfully created.")
+                                msg.setWindowTitle("Register")
+                                msg.exec_()
+
+                                self.gotologin()
+                            except:
+                                self.errorMessage.setText("Email has already been taken.")
+                                self.errorMessage.setVisible(True)
+                    else:
+                        self.errorMessage.setText("Passwords doesn't match!")
+                        self.errorMessage.setVisible(True)
 
     def gotologin(self):
         login = Login()
@@ -363,14 +370,15 @@ class Worker(QThread):
                                   'Date: ' + time.strftime("%d-%b-%Y") + '\nTime: ' + time.strftime("%H:%M:%S %p")
 
                         # Send email here
-                        sender_Email = "facemaskdetector2021@gmail.com"
-                        reciever_Email = self.email
-                        password = "Facemask123"
-                        self.sendEmail(sender_Email, "hongkailo2000@gmail.com", password, img_name, content)
-                        print("Email sent!")
+                        if is_internet():
+                            sender_Email = "facemaskdetector2021@gmail.com"
+                            receiver_Email = self.email
+                            password = "Facemask123"
+                            self.sendEmail(sender_Email, receiver_Email, password, img_name, content)
+                            print("Email sent!")
 
                         # Alert message box
-                        self.messagebox("Warning", "Access Denied!\nPlease wear a Face Mask.")
+                        messagebox("Warning", "Access Denied!\nPlease wear a Face Mask.")
 
             # show the video
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -378,29 +386,20 @@ class Worker(QThread):
             Pic = ConvertToQtFormat.scaled(1280, 720, 1)
             self.ImageUpdate.emit(Pic)
 
-    def messagebox(self, title, text):
-        root = tkinter.Tk()
-        root.withdraw()
-        tkinter.messagebox.showwarning(title, text)
-        root.destroy()
-
     def sendEmail(self, sender, receiver, password, attachment, content):
-        Sender_Email = sender
-        Reciever_Email = receiver
-        Password = password
         newMessage = EmailMessage()
         newMessage['Subject'] = "Face Mask Detector System"
-        newMessage['From'] = Sender_Email
-        newMessage['To'] = Reciever_Email
-        newMessage.set_content(content)
+        newMessage['From'] = sender
+        newMessage['To'] = receiver
         files = [attachment]
+        newMessage.set_content(content)
         for file in files:
             with open(file, 'rb') as f:
                 file_data = f.read()
                 file_name = f.name
             newMessage.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
         with smtplib.SMTP_SSL('smtp.gmail.com', 465)  as smtp:
-            smtp.login(Sender_Email, Password)
+            smtp.login(sender, password)
             smtp.send_message(newMessage)
 
     def run(self):
@@ -429,12 +428,7 @@ def messagebox(title, text):
 
 
 # //////////////////////////////////////////   RUN HERE   //////////////////////////////////////////
-while not is_internet():
-    messagebox("No internet", "Try:\n\n-Checking the network cables, modem, and router\n-Reconnecting to Wi-Fi")
-    print("Internet disconnected")
-
 if is_internet():
-    print("Internet is active")
     app = QApplication(sys.argv)
     mainWindow = Login()
     widget = QtWidgets.QStackedWidget()
@@ -444,3 +438,7 @@ if is_internet():
     widget.setFixedHeight(960)
     widget.show()
     app.exec_()
+else:
+    messagebox("No internet", "Try:\n\n-Checking the network cables, modem, and router\n-Reconnecting to Wi-Fi")
+    print("Internet disconnected")
+    # sys.exit()
