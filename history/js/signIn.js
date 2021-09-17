e40b3ff86bcfc2ad4d7e9ddcea95bbf2cac7ffd3
Hongkai <hongkailo2000@gmail.com>
2021-09-18 01:47:36 +0800

    Able to auth the user and view the history for selected day.

--- history/js/signIn.js
+++ history/js/signIn.js
@@ -1,3 +1,16 @@
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
 var firebaseConfig = {
     apiKey: "AIzaSyBZRN-qaq0NluYrPUxg6l1m2ppmpVQt4-0",
     authDomain: "face-mask-detection-2021-8710d.firebaseapp.com",
@@ -13,26 +26,103 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 function signUp() {
-    var email = document.getElementById("signUpEmail");
-    var pass = document.getElementById("signUpPassword");
+    var email = document.getElementById("signUpEmail").value;
+    var pass = document.getElementById("signUpPassword").value;
+    var confirmPass = document.getElementById("signUpConfirmPassword").value;
+    // (?=.*\d)          // should contain at least one digit
+    // (?=.*[a-z])       // should contain at least one lower case
+    // (?=.*[A-Z])       // should contain at least one upper case
+    // [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
+    // (?=.*[!@#$%^&*])  // should contain at least one special character
+    var vpass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
 
-    const promise = auth.createUserWithEmailAndPassword(email.value, pass.value);
-    promise.catch(e => alert(e.message));
-
-    alert("Sign up");
+    // compare password
+    if (email != "" && pass != "" && confirmPass != "") {
+        if (pass != confirmPass) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: "Passwords doesn't match"
+            });
+        } else {
+            // check password length
+            if (pass.length < 8) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: "Password must have at least 8 characters"
+                });
+            } else {
+                //check password format
+                if (!pass.match(vpass)) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        html: "<p>Passwords should contain: <br> -at least one digit" +
+                            "<br> -at least one lower case <br> -at least one upper case" +
+                            "<br> -at least one special character</p>"
+                    });
+                } else {
+                    // sign up here
+                    auth.createUserWithEmailAndPassword(email, pass).catch(function (error) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            html: error.message
+                        })
+                    }).then(function (user) {
+                        if (user) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Welcome',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        }
+                    })
+                }
+            }
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: "All fields cannot be empty."
+        });
+    }
 }
 
 function signIn() {
-    var email = document.getElementById("signInEmail");
-    var pass = document.getElementById("signInPassword");
-
-    const promise = auth.signInWithEmailAndPassword(email.value, pass.value);
-    promise.catch(e => alert(e.message));
-    if (user) {
-        alert("sign in");
-        var uid = user.uid;
-        window.location.href = 'history.html?id=' + uid;
+    var email = document.getElementById("signInEmail").value;
+    var pass = document.getElementById("signInPassword").value;
+    // const promise = auth.signInWithEmailAndPassword(email.value, pass.value);
+    // promise.catch(e => alert(e.message));
+    if (email != "" && pass != "") {
+        auth.signInWithEmailAndPassword(email, pass).catch(function (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                html: error.message
+            })
+        }).then(function (user) {
+            if (user) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Welcome',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: "All fields cannot be empty."
+        });
     }
+
+
 }
 
 function signOut() {
@@ -43,9 +133,14 @@ function signOut() {
 //active user to homepage
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+        console.log("loged in");
         var uid = user.uid;
-        window.location.href = 'history.html?id=' + uid;
+        setTimeout(function () {
+            window.location.href = 'history.html';
+        }, 2000)
+
     } else {
+        console.log("No user")
         // alert("No Active user Found")
     }
 })
